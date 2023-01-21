cc9e70d79da83c33cac5875602b4971d34b6905a
Ruslana <bronnikovars@yandex.ru>
2023-01-21 16:58:47 +0300

    commit 3 after work on mistake

--- script/scrtipt.js
+++ script/scrtipt.js
@@ -1,21 +1,21 @@
 const popup = document.querySelector('.popup');
-const popupForm = document.querySelector('.popup__body');
+const popupForm = document.querySelector('.popup__container');
 const popupOpen = document.querySelector('.profile__button_act_edit');
 const popupClose = document.getElementById('close')
 const popupSave = document.getElementById('save')
 const profileName = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__subtitle');
-const inputName = document.querySelector('.popup__input_name');
-const inputDescription = document.querySelector('.popup__input_career');
+const inputName = document.querySelector('.popup__input_name_value');
+const inputDescription = document.querySelector('.popup__input_career_value');
 
 function openPopup() {
-  popup.classList.add('popup__opene');
+  popup.classList.add('popup_opene');
   inputName.value = profileName.textContent;
   inputDescription.value = profileDescription.textContent;
 }
 
 function closePopup() {
-  popup.classList.remove('popup__opene');
+  popup.classList.remove('popup_opene');
 }
 
 function handleFormSubmit(evt) {
